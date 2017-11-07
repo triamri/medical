75fd63a1a675bde1a80968da36ea3c9111708410
febriliando <febriliandoputra@gmail.com>
2017-11-07 21:58:49 +0700

    NVC first

--- routers/pasien.js
+++ routers/pasien.js
@@ -1,8 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const model = require ('../models');
 
 router.get('/', function (req, res) {
-    res.send('pasien');
+  model.Pasien.findAll({
+    order : [['id','ASC']]
+  }).then((rows)=> {
+    res.render('pasiens', {rows : rows})
+  }).catch((err) => {
+    res.redirect('/pasiens')
+  })
+})
+
+router.get('/add', (req, res) => {
+      res.render('pasienAdd');
+})
+
+router.post('/add', (req, res) => {
+  model.Pasien.create(req.body).then(rows => {
+    res.redirect('/pasiens')
+  }).catch(err => {
+    res.send(err);
+  })
+  // res.send(req.body);
+})
+
+router.get('/edit/:id', (req,res) => {
+  // res.send('test')
+  model.Pasien.findById(req.params.id).then(rows => {
+      res.render('pasienEdit', { rows });
+  }).catch(err => {
+    console.log(err);
+  })
 });
+
+router.post('/edit/:id', (req,res) => {
+  model.Pasien.update(req.body,{
+    where : {
+      id : req.params.id
+    }
+  }).then(data => {
+    res.redirect('/pasiens');
+  }).catch(err => {
+    res.send(err);
+  });
+})
+
+
+router.get('/delete/:id', (req, res) => {
+  model.Pasien.destroy({
+    where: {
+      id : req.params.id
+    }
+  })
+  .then(() => {
+    res.redirect('/pasiens')
+  })
+  .catch(err => {
+    res.send(err)
+  })
+})
+
 
 module.exports = router;
